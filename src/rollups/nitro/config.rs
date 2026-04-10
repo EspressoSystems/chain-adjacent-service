291533de73ff5352d153adf1f6b24bec5b50a376
Jeremy <yiliang.he@qq.com>
2026-04-10 15:35:44 +0800

    feat: build hotshot transactions with size limit

--- src/rollups/nitro/config.rs
+++ src/rollups/nitro/config.rs
@@ -6,6 +6,7 @@ use crate::rollups::nitro::feed::relay::FeedConfig;
 #[derive(Debug, Clone, Deserialize, Default)]
 pub struct NitroConfig {
     pub legacy_signer_addresses: Vec<Address>,
+    pub sequencer_addresses: Vec<Address>,
     pub chain_id: u64,
 
     pub feed_config: FeedConfig,
