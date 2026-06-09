2e4012c380cbcc07c7963d1acc95c57e3f0af9fd
Varun Doshi <doshivarun202@gmail.com>
2026-06-09 12:05:57 -0400

    fix: fmt

--- src/submitter/submitter.rs
+++ src/submitter/submitter.rs
@@ -156,7 +156,10 @@ where
                     let finalized = loop {
                         match client.fetch_transaction_by_hash(tx_hash).await {
                             Ok(data) => {
-                                tracing::info!("finalized transaction with hash: {}", hex::encode(&data.hash));
+                                tracing::info!(
+                                    "finalized transaction with hash: {}",
+                                    hex::encode(data.hash)
+                                );
                                 break true;
                             }
                             Err(err) => {
