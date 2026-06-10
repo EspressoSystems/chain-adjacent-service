4d96d56be64a57557a0b7aef5ea2782a73ef1ead
Varun Doshi <doshivarun202@gmail.com>
2026-06-10 11:46:48 -0400

    fix: espresso hash display

--- src/submitter/submitter.rs
+++ src/submitter/submitter.rs
@@ -156,10 +156,7 @@ where
                     let finalized = loop {
                         match client.fetch_transaction_by_hash(tx_hash).await {
                             Ok(data) => {
-                                tracing::info!(
-                                    "finalized transaction with hash: {}",
-                                    hex::encode(data.hash)
-                                );
+                                tracing::info!("finalized transaction with hash: {}", data.hash);
                                 break true;
                             }
                             Err(err) => {
